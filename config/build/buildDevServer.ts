15bbf3f3493afbde47bbd401ec44edeeb3cabd8f
Ayan <anarbajaan1@gmail.com>
2025-03-12 11:23:25 +0500

    added HMR plugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,10 +1,12 @@
 import { Configuration as devServerConfiguration } from "webpack-dev-server";
 import { BuildOptions } from "./types/config";
+import { truncate } from "fs";
 
 export function builDevServer(options: BuildOptions): devServerConfiguration{
   return {
     port: options.port,
     open: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    hot: true
   }
 }
